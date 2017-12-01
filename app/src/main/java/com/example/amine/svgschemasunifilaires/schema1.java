0db9ff5ff49e0a6a4bef63a0ba94a13f13ba163d
Enemak <amine_makni@hotmail.fr>
2017-12-01 10:51:09 +0100

    Charger les images Svg depuis le web

--- app/src/main/java/com/example/amine/svgschemasunifilaires/schema1.java
+++ app/src/main/java/com/example/amine/svgschemasunifilaires/schema1.java
@@ -1,13 +1,21 @@
 package com.example.amine.svgschemasunifilaires;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGImageView;
 import com.caverock.androidsvg.SVGParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -20,28 +28,62 @@ public class schema1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schema1);
 
+
+        LoadURITask LoadURITask = new LoadURITask();
         try {
-            SVG svg = SVG.getFromResource(this, R.raw.exemple1);
 
-            SVGImageView svgImageView = new SVGImageView(this);
-            pAttacher = new PhotoViewAttacher(svgImageView);
-            pAttacher.update();
-            svgImageView.setSVG(svg);
-            svgImageView.setLayoutParams(
-                    new ViewGroup.LayoutParams(
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            ViewGroup.LayoutParams.MATCH_PARENT));
+            URL url = new URL("https://upload.wikimedia.org/wikipedia/commons/b/b1/741_op-amp_schematic.svg");
+            LoadURITask.execute(url);
 
-            RelativeLayout layout =
-                    (RelativeLayout) findViewById(R.id.layout_card);
+        } catch (MalformedURLException e) {
 
-            layout.addView(svgImageView);
+            Toast.makeText(getApplicationContext(), getString(R.string.malformed_url), Toast.LENGTH_LONG).show();
+        }
+    }
 
-        } catch (SVGParseException e){
-            e.printStackTrace();
+        private class LoadURITask extends AsyncTask<URL, Integer, SVG> {
+
+            public LoadURITask() {}
+
+            protected SVG doInBackground(URL... urls) {
+                URL url = urls[0];
+                InputStream is = null;
+
+                try {
+                    is = url.openConnection().getInputStream();
+                    SVG svg = SVG.getFromInputStream(is);
+                    return svg;
+                } catch (SVGParseException e) {
+                    Log.e("SVGImageView", "Parse error loading URI: " + e.getMessage());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        is.close();
+                    } catch (IOException e) { /* do nothing */ }
+                }
+                return null;
+            }
+
+            protected void onPostExecute(SVG svg) {
+
+                if (svg != null) {
+                    SVGImageView svgImageView = new SVGImageView(getApplicationContext());
+                    pAttacher = new PhotoViewAttacher(svgImageView);
+                    pAttacher.update();
+                    svgImageView.setSVG(svg);
+                    svgImageView.setLayoutParams(
+                            new ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.MATCH_PARENT,
+                                    ViewGroup.LayoutParams.MATCH_PARENT));
+
+                    RelativeLayout layout =
+                            (RelativeLayout) findViewById(R.id.layout_card);
+
+                    layout.addView(svgImageView);
+
+                }
+            }
         }
 
-
-
-    }
 }
--- app/src/main/java/com/example/amine/svgschemasunifilaires/schema2.java
+++ app/src/main/java/com/example/amine/svgschemasunifilaires/schema2.java
@@ -1,13 +1,20 @@
 package com.example.amine.svgschemasunifilaires;
-
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGImageView;
 import com.caverock.androidsvg.SVGParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -21,29 +28,61 @@ public class schema2 extends AppCompatActivity {
         setContentView(R.layout.activity_schema2);
 
 
-
+        LoadURITask LoadURITask = new LoadURITask();
         try {
-            SVG svg = SVG.getFromResource(this, R.raw.exemple2);
 
-            SVGImageView svgImageView = new SVGImageView(this);
-            pAttacher = new PhotoViewAttacher(svgImageView);
-            pAttacher.update();
-            svgImageView.setSVG(svg);
-            svgImageView.setLayoutParams(
-                    new ViewGroup.LayoutParams(
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            ViewGroup.LayoutParams.MATCH_PARENT));
+            URL url = new URL("https://upload.wikimedia.org/wikipedia/commons/7/78/ElectronicOscillator_Clapp-JFET-D.svg");
+            LoadURITask.execute(url);
 
-            RelativeLayout layout =
-                    (RelativeLayout) findViewById(R.id.layout_card);
+        } catch (MalformedURLException e) {
 
-            layout.addView(svgImageView);
+            Toast.makeText(getApplicationContext(), getString(R.string.malformed_url), Toast.LENGTH_LONG).show();
+        }
+    }
 
-        } catch (SVGParseException e){
-            e.printStackTrace();
+    private class LoadURITask extends AsyncTask<URL, Integer, SVG> {
+
+        public LoadURITask() {}
+
+        protected SVG doInBackground(URL... urls) {
+            URL url = urls[0];
+            InputStream is = null;
+
+            try {
+                is = url.openConnection().getInputStream();
+                SVG svg = SVG.getFromInputStream(is);
+                return svg;
+            } catch (SVGParseException e) {
+                Log.e("SVGImageView", "Parse error loading URI: " + e.getMessage());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) { /* do nothing */ }
+            }
+            return null;
         }
 
+        protected void onPostExecute(SVG svg) {
 
+            if (svg != null) {
+                SVGImageView svgImageView = new SVGImageView(getApplicationContext());
+                pAttacher = new PhotoViewAttacher(svgImageView);
+                pAttacher.update();
+                svgImageView.setSVG(svg);
+                svgImageView.setLayoutParams(
+                        new ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.MATCH_PARENT));
 
+                RelativeLayout layout =
+                        (RelativeLayout) findViewById(R.id.layout_card);
+
+                layout.addView(svgImageView);
+
+            }
+        }
     }
+
 }
